b0029462f0d4422e673acf228d8ea6c0e2965b50
Israel HV <isrraelhuaman@gmail.com>
2019-11-18 17:53:58 -0500

    COmmit inicial

---
+++ config/yargs.js
@@ -0,0 +1,31 @@
+
+
+
+const opts = {
+    base:{
+        demand:true ,
+        alias: 'b'
+    },
+    limite: {
+        alias: 'l',
+        default: 10
+    }
+}
+
+
+const argv = require('yargs')
+.command('listar', 
+'Imprime en consola la tabla de multplicar', opts)
+
+.command('crear', 
+'genera un archivo con la tabla de multiplicar',opts)
+    .help()
+    .argv ;
+
+
+    module.exports = {
+
+        argv
+    }
+
+
